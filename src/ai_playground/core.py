8ed0de5a7c9eb285276b45de389cf5900108339e
isaacabderman-creator <hei.isaac.2@gmail.com>
2026-09-12 19:06:35 +0300

    feat: create GeminiClient class

--- src/ai_playground/core.py
+++ src/ai_playground/core.py
@@ -1,9 +1,7 @@
-import json
 import os
 
 import httpx
 from httpx import Response, URL, HTTPError, HTTPStatusError
-from pathlib import Path
 
 import dotenv
 
@@ -11,33 +9,35 @@ dotenv.load_dotenv()
 
 MODEL_API_URL = os.getenv("MODEL_API_URL")
 MODEL_API_KEY = os.getenv("MODEL_API_KEY")
-OUTPUT_PATH = Path(__file__).parent / "sample_reply.json"
 
 
-def reply(prompt: str) -> dict:
-    body = {
-        "contents": [{"parts": [{"text": prompt}]}],
-    }
-    if not MODEL_API_URL and MODEL_API_KEY:
-        raise NameError("Model API url/key not provided")
+class GeminiClient:
+    def __init__(self):
+        self.client = httpx.Client()
 
-    try:
-        response: Response = httpx.post(
-            MODEL_API_URL,
-            headers={
-                "x-goog-api-key": MODEL_API_KEY,
-                "Content-Type": "application/json",
-            },
-            json=body,
-        )
-        return response.raise_for_status().json()
-    except HTTPStatusError as e:
-        raise RuntimeError(f"HTTP Status Error: {e}")
-    except HTTPError as e:
-        raise RuntimeError(f"HTTP Error: {e}")
+    def reply(self, prompt: str) -> dict:
+        body = {
+            "contents": [{"parts": [{"text": prompt}]}],
+        }
+        if not (MODEL_API_URL and MODEL_API_KEY):
+            raise NameError("Model API url/key not provided")
+
+        try:
+            response: Response = self.client.post(
+                MODEL_API_URL,
+                headers={
+                    "x-goog-api-key": MODEL_API_KEY,
+                    "Content-Type": "application/json",
+                },
+                json=body,
+            )
+            return response.raise_for_status().json()
+        except HTTPStatusError as e:
+            raise RuntimeError(f"HTTP Status Error: {e}")
+        except HTTPError as e:
+            raise RuntimeError(f"HTTP Error: {e}")
 
 
 if __name__ == "__main__":
-    answer = reply(input("User: "))
-
-    OUTPUT_PATH.write_text(json.dumps(answer, indent=2))
+    client = GeminiClient()
+    print(client.reply("Hello World"))

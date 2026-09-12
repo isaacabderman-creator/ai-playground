6dc6acf437a56a5ac44f2a0b0e6d927000295b81
isaacabderman-creator <hei.isaac.2@gmail.com>
2026-09-12 20:13:24 +0300

    feat: implement looping chat

--- src/ai_playground/core.py
+++ src/ai_playground/core.py
@@ -24,23 +24,40 @@ class GeminiClient:
                 "Content-Type": "application/json",
             },
         )
+        self.context = {"contents": []}
 
     def reply(self, prompt: str) -> dict:
-        body = {
-            "contents": [{"parts": [{"text": prompt}]}],
-        }
+        _part = {"role": "user", "parts": [{"text": prompt}]}
+        self.context["contents"].append(_part)
         try:
-            response: Response = self.client.post(
-                f"/models/{self.model}:generateContent",
-                json=body,
+            _response: Response = self.client.post(
+                f"/models/{self.model}:generateContent", json=self.context
             )
-            return response.raise_for_status().json()
-        except HTTPStatusError as e:
-            raise RuntimeError(f"HTTP Status Error: {e}")
-        except HTTPError as e:
-            raise RuntimeError(f"HTTP Error: {e}")
+            return _response.raise_for_status().json()
+        except HTTPStatusError as error:
+            raise RuntimeError(f"HTTP Status Error: {error}")
+        except HTTPError as error:
+            raise RuntimeError(f"HTTP Error: {error}")
+
+    def insert_answer(self, answer: str) -> None:
+        _part = {"role": "model", "parts": [{"text": answer}]}
+        self.context["contents"].append(_part)
 
 
 if __name__ == "__main__":
     client = GeminiClient()
-    print(client.reply("Hello World"))
+    while True:
+        try:
+            prompt = input("user > ")
+            response = client.reply(prompt)
+        except EOFError:
+            sys.exit(0)
+        except RuntimeError as e:
+            print(e)
+            continue
+        try:
+            answer = response["candidates"][0]["content"]["parts"][0]["text"]
+            client.insert_answer(answer)
+            print("model > ", answer)
+        except Exception as e:
+            print(e)

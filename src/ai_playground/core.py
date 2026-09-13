bcd8e29faa34fec318506a6978c6ed9968d2c4d0
isaacabderman-creator <hei.isaac.2@gmail.com>
2026-09-13 16:09:30 +0300

    ref: move answering context update into reply

--- src/ai_playground/core.py
+++ src/ai_playground/core.py
@@ -26,22 +26,23 @@ class GeminiClient:
         )
         self.context = {"contents": []}
 
-    def reply(self, prompt: str) -> dict:
+    def reply(self, prompt: str) -> str:
         _part = {"role": "user", "parts": [{"text": prompt}]}
         self.context["contents"].append(_part)
         try:
             response: Response = self.client.post(
                 f"/models/{self.model}:generateContent", json=self.context
             )
-            return response.raise_for_status().json()
+            data = response.raise_for_status().json()
         except HTTPStatusError as error:
             raise RuntimeError(f"HTTP Status Error: {error}")
         except HTTPError as error:
             raise RuntimeError(f"HTTP Error: {error}")
+        answer = data["candidates"][0]["content"]["parts"][0]["text"]
 
-    def insert_answer(self, answer: str) -> None:
         part = {"role": "model", "parts": [{"text": answer}]}
         self.context["contents"].append(part)
+        return answer
 
     def __enter__(self):
         return self
@@ -55,18 +56,13 @@ def main() -> None:
         while True:
             try:
                 prompt = input("user > ")
-                response = client.reply(prompt)
+                answer = client.reply(prompt)
             except (EOFError, KeyboardInterrupt):
                 sys.exit(0)
             except RuntimeError as e:
                 print(e)
                 continue
-            try:
-                answer = response["candidates"][0]["content"]["parts"][0]["text"]
-                client.insert_answer(answer)
-                print("model > ", answer)
-            except Exception as e:
-                print(e)
+            print("model > ", answer)
 
 
 if __name__ == "__main__":
